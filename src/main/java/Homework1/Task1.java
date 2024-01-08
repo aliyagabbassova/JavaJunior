cf62a40314c97632a61b8b16b076247f26735bde
aliya <aliya.gabbassova1911@gmail.com>
2024-01-08 14:19:24 +0600

    task1

--- src/main/java/Homework1/Task1.java
+++ src/main/java/Homework1/Task1.java
@@ -1,2 +1,18 @@
-package Homework1;public class Task1 {
+package Homework1;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class Task1 {
+    public static void main(String[] args) {
+        List<Integer> numbers = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
+
+        double average = numbers.stream()
+                .filter(n -> n % 2 == 0)
+                .mapToDouble(Integer::doubleValue)
+                .average()
+                .orElse(0.0);
+
+        System.out.println("Среднее значение четных чисел: " + average);
+    }
 }
